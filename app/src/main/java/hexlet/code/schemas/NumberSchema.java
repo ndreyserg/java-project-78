69161787d75e23894f846c22378a1ac02f546061
ndreyserg <ndreyserg@gmail.com>
2026-01-27 08:02:06 +0500

    Проверка на null

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -9,12 +9,12 @@ public final class NumberSchema extends BaseSchema<Integer> {
     }
 
     public NumberSchema positive() {
-        this.addRule("positive", n -> n > 0);
+        this.addRule("positive", n -> Objects.nonNull(n) && n > 0);
         return this;
     }
 
     public NumberSchema range(int min, int max) {
-        this.addRule("range", n -> n >= min && n <= max);
+        this.addRule("range", n -> Objects.nonNull(n) && n >= min && n <= max);
         return this;
     }
 }
